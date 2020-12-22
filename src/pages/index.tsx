fab5da66624b99535d2266e1d31cec225be45cf2
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2020-12-22 12:31:16 +0100

    Add typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
+const styles = require('../styles/Home.module.css'); //issue with css module support in ts?
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>src/pages/index.js</code>
+          <code className={styles.code}>src/pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
